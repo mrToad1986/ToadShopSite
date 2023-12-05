39136c9256bb144f930dd55cb4845daee985847d
mrToad1986 <korostylev.ov@gmail.com>
2023-12-05 22:45:43 +0300

    add model Regions

--- mainapp/models.py
+++ mainapp/models.py
@@ -7,3 +7,12 @@ class ListOfCountries (models.Model):
 
     def __true__(self):
         return self.name
+
+class Regions(models.Model):
+    country = models.ForeignKey(ListOfCountries, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+    is_active = models.BooleanField(verbose_name='активна', default=True)
+
+    def __str__(self):
+        return self.name
