c23bf51655d9f23274d4612ede1deef8d07ed301
mrToad1986 <korostylev.ov@gmail.com>
2023-12-05 22:43:15 +0300

    add model ListOfCountries

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ListOfCountries (models.Model):
+    name = models.CharField(verbose_name='название', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+    is_active = models.BooleanField(verbose_name='активно', default=True)
+
+    def __true__(self):
+        return self.name
