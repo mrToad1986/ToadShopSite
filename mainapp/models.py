8411ab7c95b1022bb30cfba7a3a8113ef7888e1a
mrToad1986 <korostylev.ov@gmail.com>
2023-12-21 21:42:01 +0300

    add mainapp models

--- mainapp/models.py
+++ mainapp/models.py
@@ -18,7 +18,13 @@ class Regions(models.Model):
         return self.name
 
 class Species(models.Model):
-    pass
+    region  = models.ForeignKey(Regions, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='название', max_length=64, unique=True)
+    short_desc = models.CharField(verbose_name='краткое описание', max_length=96, blank=True)
+    full_desc = models.TextField(verbose_name='полное описание', blank=True)
+    image = models.ImageField(upload_to='images', blank=True)
+    price = models.DecimalField(verbose_name='cтоимость', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='в наличии', default=0)
 
 class Genus(models.Model):
     pass
