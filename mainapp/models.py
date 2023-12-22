14d0872728650e8633b724f447da95265fbaceb7
mrToad1986 <korostylev.ov@gmail.com>
2023-12-22 10:39:08 +0300

    delete mainapp models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,22 +1,5 @@
 from django.db import models
 
-class ListOfCountries (models.Model):
-    name = models.CharField(verbose_name='название', max_length=64, unique=True)
-    description = models.TextField(verbose_name='описание', blank=True)
-    is_active = models.BooleanField(verbose_name='активно', default=True)
-
-    def __str__(self):
-        return self.name
-
-class Regions(models.Model):
-    country = models.ForeignKey(ListOfCountries, on_delete=models.CASCADE)
-    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
-    description = models.TextField(verbose_name='описание', blank=True)
-    is_active = models.BooleanField(verbose_name='активна', default=True)
-
-    def __str__(self):
-        return self.name
-
 class Species(models.Model):
     region  = models.ForeignKey(Regions, on_delete=models.CASCADE)
     name = models.CharField(verbose_name='название', max_length=64, unique=True)
@@ -25,12 +8,3 @@ class Species(models.Model):
     image = models.ImageField(upload_to='images', blank=True)
     price = models.DecimalField(verbose_name='cтоимость', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='в наличии', default=0)
-
-class Genus(models.Model):
-    pass
-
-class Feed(models.Model):
-    pass
-
-class User(models.Model):
-    pass
