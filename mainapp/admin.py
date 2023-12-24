bb70c90cd2ab693f5f7375d9c9900a54ea51dfb3
mrToad1986 <korostylev.ov@gmail.com>
2023-12-24 21:26:02 +0300

    standard admin panel

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Species, Product
+
+admin.site.register(Category)
+admin.site.register(Species)
+admin.site.register(Product)
 
 # Register your models here.
