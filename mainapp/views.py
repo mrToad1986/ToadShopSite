2777826fd3b6f85914f36e22a27e61f828729858
mrToad1986 <korostylev.ov@gmail.com>
2024-01-09 18:35:16 +0300

    add mainapp views

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Species, Product
 
-# Create your views here.
+
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+
+# установить django-stubs или использовать models.Manager()
+def species(request):
+    title = 'животные'
+    list_of_species = Species.objects.all()
+    content = {
+        'title': title,
+        'list_of_species': list_of_species,
+    }
+    return render(request, 'mainapp/species.html', content)
+
+
+def products(request):
+    pass
+
+def species_detailed(request, pk):
+    title = 'животные'
+    content = {
+        'title': title,
+        'details': get_object_or_404(Species, pk=pk),
+    }
+    return render(request, 'mainapp/species_detailed.html', content)
+
+def products_detailed(requet):
+    pass
