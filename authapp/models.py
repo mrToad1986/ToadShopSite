2173b58b6c852c8b58fa591f19989d4341cf720d
mrToad1986 <korostylev.ov@gmail.com>
2024-02-04 20:20:59 +0300

    add authapp models, make migrations

--- authapp/models.py
+++ authapp/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='user_avatars', blank=True) #добавить default
+    nickname = models.CharField(verbose_name='никнэйм', max_length=64, blank=True)
     age = models.PositiveIntegerField(verbose_name='возраст', default=21)
 
 class ShopUserProfile(models.Model):
     user = models.OneToOneField(
-        ShopUser, unique=True, null=False, db_index=True, on_delete=models.CASCADE()
+        ShopUser, unique=True, null=False, db_index=True, on_delete=models.CASCADE
     )
     tagline = models.CharField(verbose_name='интересы', max_length=256, blank=True)
     aboutMe = models.CharField(verbose_name='о себе', max_length=512, blank=True)
     city = models.CharField(verbose_name='город', max_length=64, blank=True)
     country = models.CharField(verbose_name='страна', max_length=64, blank=True)
-    #добавить дату регистрации
