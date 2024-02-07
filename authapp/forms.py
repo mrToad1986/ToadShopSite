233f34872fffbad3bfaf336f134a5092156321cc
mrToad1986 <korostylev.ov@gmail.com>
2024-02-07 19:56:17 +0300

    add authapp forms editform

--- authapp/forms.py
+++ authapp/forms.py
@@ -31,3 +31,31 @@ def check_age(self):
     if data < 12:
         raise forms.ValidationError('Сайт предназначен для пользователей старше 12 лет')
     return data
+
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = (
+            # 'username', #from AbstractUser
+            'first_name',  # from AbstractUser
+            'last_name',  # from AbstractUser
+            'email',  # from AbstractUser
+            'password1',
+            'password2',
+            'nickname'  # from ShopUser переоп. Username
+            'age',  # from ShopUser
+            'avatar'  # from ShopUser
+        )
+
+    def __init__ (self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text == ''
+            if field_name == 'password':
+                field.widget = forms.HiddenInput()
+
+    def check_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise forms.ValidationError('Сайт предназначен для пользователей старше 12 лет')
