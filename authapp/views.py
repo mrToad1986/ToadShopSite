60ca7ab50a8588e9cac4cc6d6c21ead4db98285b
mrToad1986 <korostylev.ov@gmail.com>
2024-02-10 20:23:31 +0300

    add authapp views

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from .forms import ShopUserRegisterForm
 
-# Create your views here.
+def register (request):
+    title = 'регистрация'
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+    if register_form.is_valid():
+        register_form.save()
+        return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        register_form = ShopUserRegisterForm()
+    content = {'title': title, 'register_form': register_form}
+    return render(request, 'authapp/register.html', content)
