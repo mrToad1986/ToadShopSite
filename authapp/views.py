90b16d280a8be068755e56b24a01f339f869026c
mrToad1986 <korostylev.ov@gmail.com>
2024-02-11 19:17:25 +0300

    fix authapp register view

--- authapp/views.py
+++ authapp/views.py
@@ -6,10 +6,13 @@ def register (request):
     title = 'регистрация'
     if request.method == 'POST':
         register_form = ShopUserRegisterForm(request.POST, request.FILES)
-    if register_form.is_valid():
-        register_form.save()
-        return HttpResponseRedirect(reverse('auth:login'))
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
     else:
         register_form = ShopUserRegisterForm()
-    content = {'title': title, 'register_form': register_form}
+    content = {
+        'title': title,
+        'register_form': register_form
+    }
     return render(request, 'authapp/register.html', content)
