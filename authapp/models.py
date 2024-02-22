bc6b6063858247e66bd1e03b5dbe0ddf6eaf7807
mrToad1986 <korostylev.ov@gmail.com>
2024-02-22 12:01:02 +0300

    add autofill ShopUserProfile table

--- authapp/models.py
+++ authapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='user_avatars', blank=True) #добавить default
@@ -14,3 +16,13 @@ class ShopUserProfile(models.Model):
     aboutMe = models.CharField(verbose_name='о себе', max_length=512, blank=True)
     city = models.CharField(verbose_name='город', max_length=64, blank=True)
     country = models.CharField(verbose_name='страна', max_length=64, blank=True)
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.ShopUserProfile.save()
+
