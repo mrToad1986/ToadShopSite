15cf026b15612930f2b726615d80eae7be5e5634
mrToad1986 <korostylev.ov@gmail.com>
2025-04-08 15:13:05 +0300

    fix autorun

--- runserver.bat
+++ runserver.bat
@@ -1,2 +1,10 @@
+echo запоминаем рабочую директорию
+pushd %CD%
+echo переходим в директорию виртуального окружения
+cd ..
+echo активируем вирутальное окружение
+call "venv\scripts\activate.bat"
+echo возвращаемся обратно
+popd
 python manage.py runserver
 pause
